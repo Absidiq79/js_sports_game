acc1cf81dee22723749639e8c23419384ac1fb4c
Abubakarr Turay <abubakarrturay@Abubakarrs-MBP.attlocal.net>
2020-09-21 23:37:54 -0400

    initial commit

---
+++ js_sports_game/assets/javascript/game.js
@@ -0,0 +1,46 @@
+let teamoneShootButton = document.querySelector("#teamone-shoot-button")
+let teamoneNumshots = document.querySelector("#teamone-numshots")
+let teamoneNumgoals= document.querySelector("#teamone-numgoals")
+teamoneShootButton.addEventListener("click",function () {
+    console.log("teamone Shoot Button");
+})
+let newShottotal = Number(teamoneNumshots.innerHTML) + 1;
+teamoneNumshots.innerHTML = newShottotal;
+ 
+if(Math.random() < 0.5) {
+   console.log("goal score")
+let newgoals = Number(teamoneNumgoals.innerHTML) + 1;
+teamoneNumgoals.innerHTML = newgoals;
+
+
+
+let teamtwoShootButton = document.querySelector("#teamtwo-shoot-button")
+let teamtwoNumshots = document.querySelector("#teamtwo-numshots")
+let teamtwoNumgoals= document.querySelector("#teamtwo-numgoals")
+
+teamtwoShootButton.addEventListener("click",function () {
+console.log("teamtwo shoot button");
+})
+let newShottotal = Number(teamtwoNumshots.innerHTML) + 1;
+teamtwoNumshots.innerHTML = newShottotal;
+if(Math.random() < 0.5) {
+   console.log("goal score")
+
+let newgoals = Number(teamtwoNumgoals.innerHTML) + 1;
+teamtwoNumgoals.innerHTML= newgoals;
+
+}
+
+let numReset =document.querySelector("#num-resets")
+let resetButton =document.querySelector("#reset-Button")
+resetButton.addEventListener("click", function () {
+
+})
+   console.log("reset Button")
+   teamoneNumshots.innerHTML = 0;
+   teamoneNumgoals.innerHTML = 0;
+   teamtwoNumshots.innerHTML = 0;
+   teamtwoNumgoals.innerHTML = 0;
+let newresetNum = Number(numResets.innerHTML) + 1;
+numResets.innerHTML = newresetNum;
+}
