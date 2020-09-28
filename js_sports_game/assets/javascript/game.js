6e0d550042c85b92c9bd89f5ac91193ba1880cbb
Abubakarr Turay <abubakarrturay@Abubakarrs-MBP.attlocal.net>
2020-09-28 09:01:01 -0400

    initial commit

--- js_sports_game/assets/javascript/game.js
+++ js_sports_game/assets/javascript/game.js
@@ -1,46 +1,33 @@
+
 let teamoneShootButton = document.querySelector("#teamone-shoot-button")
 let teamoneNumshots = document.querySelector("#teamone-numshots")
 let teamoneNumgoals= document.querySelector("#teamone-numgoals")
-teamoneShootButton.addEventListener("click",function () {
-    console.log("teamone Shoot Button");
+teamoneShootButton.addEventListener("click", function () {
+   let newShottotal = Number(teamoneNumshots.innerHTML) + 1;
+   teamoneNumshots.innerHTML = newShottotal;
+   if(Math.random() < 0.5) {
+      let newgoals = Number(teamoneNumgoals.innerHTML) + 1;
+      teamoneNumgoals.innerHTML = newgoals;
+      }
 })
-let newShottotal = Number(teamoneNumshots.innerHTML) + 1;
-teamoneNumshots.innerHTML = newShottotal;
- 
-if(Math.random() < 0.5) {
-   console.log("goal score")
-let newgoals = Number(teamoneNumgoals.innerHTML) + 1;
-teamoneNumgoals.innerHTML = newgoals;
-
-
-
 let teamtwoShootButton = document.querySelector("#teamtwo-shoot-button")
 let teamtwoNumshots = document.querySelector("#teamtwo-numshots")
 let teamtwoNumgoals= document.querySelector("#teamtwo-numgoals")
-
-teamtwoShootButton.addEventListener("click",function () {
-console.log("teamtwo shoot button");
+teamtwoShootButton.addEventListener("click", function () {
+   let newShottotal = Number(teamtwoNumshots.innerHTML) + 1;
+   teamtwoNumshots.innerHTML = newShottotal;
+   if(Math.random() < 0.5) {
+      let newgoals = Number(teamtwoNumgoals.innerHTML) + 1;
+      teamtwoNumgoals.innerHTML = newgoals;
+   }
 })
-let newShottotal = Number(teamtwoNumshots.innerHTML) + 1;
-teamtwoNumshots.innerHTML = newShottotal;
-if(Math.random() < 0.5) {
-   console.log("goal score")
-
-let newgoals = Number(teamtwoNumgoals.innerHTML) + 1;
-teamtwoNumgoals.innerHTML= newgoals;
-
-}
-
-let numReset =document.querySelector("#num-resets")
-let resetButton =document.querySelector("#reset-Button")
+let numResets = document.querySelector("#num-resets")
+let resetButton = document.querySelector("#reset-Button")
 resetButton.addEventListener("click", function () {
-
-})
-   console.log("reset Button")
-   teamoneNumshots.innerHTML = 0;
-   teamoneNumgoals.innerHTML = 0;
-   teamtwoNumshots.innerHTML = 0;
-   teamtwoNumgoals.innerHTML = 0;
+teamoneNumshots.innerHTML = 0;
+teamoneNumgoals.innerHTML = 0;
+teamtwoNumshots.innerHTML = 0;
+teamtwoNumgoals.innerHTML = 0;
 let newresetNum = Number(numResets.innerHTML) + 1;
 numResets.innerHTML = newresetNum;
-}
+})
